16cd2ec367b9f843bb36ce1a429676616370f354
Hernán Escudero <hernanescu@gmail.com>
2024-06-27 17:56:11 -0300

    1. Primer commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Clase de Git de PAECIS
 Este es el repo de la clase de Git de PAECIS.
+
+## Subtítulo híper creativo
+Clase de Git.
---
+++ excusator.py
@@ -0,0 +1,17 @@
+import random
+
+def generar_excusa():
+    excusas = [
+        "mi perro meó en el router.",
+        "me distraje investigando sobre el movimiento obrero en los 70.",
+        "mi gato tiró mi notebook del escritorio.",
+        "creo que me agarré Covid.",
+        "me quedé hasta las 3 AM puteándome con gente en Twitter.",
+        "todo es una construcción social, incluso hacer un commit.",
+        "Fibertel es una porquería."
+    ]
+    return random.choice(excusas)
+
+if __name__ == "__main__":
+    excusa = generar_excusa()
+    print(f"Perdón profe, no pude subir el código porque {excusa}")
